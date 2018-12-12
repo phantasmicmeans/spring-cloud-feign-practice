75661ded7a7a750ac285ae70262e75bc85a5273c
phantasmicmeans <dlaak0630@naver.com>
2018-12-13 05:53:31 +0900

    add RestController implements BookClient Interface(@FeigntClient)

---
+++ src/main/java/com/feign/practice/FeignClient/BookController.java
@@ -0,0 +1,25 @@
+package com.feign.practice.FeignClient;
+
+import com.feign.practice.domain.Book;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class BookController implements BookClient{
+
+    @Override
+    public List<Book> getBooks() {
+        return null;
+    }
+
+    @Override
+    public String getBookSpec() {
+        return null;
+    }
+
+    @Override
+    public Book update(Long bookId) {
+        return null;
+    }
+}
