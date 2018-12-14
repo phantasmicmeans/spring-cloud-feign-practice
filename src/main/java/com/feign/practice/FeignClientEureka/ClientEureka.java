b3b66ea5ac7d3076c762cfdeb112a3199433a8de
phantasmicmeans <dlaak0630@naver.com>
2018-12-15 01:36:32 +0900

    fix uri more simply - hard to understand before and complicated

--- src/main/java/com/feign/practice/FeignClientEureka/ClientEureka.java
+++ src/main/java/com/feign/practice/FeignClientEureka/ClientEureka.java
@@ -8,22 +8,25 @@ import org.springframework.web.bind.annotation.PathVariable;
 @FeignClient(value = "eureka-client")
 public interface ClientEureka { // use default feign client
 
-    @GetMapping(value = "/actuator/info")
-    ResponseEntity getDetailFromEureka(); //필요한 것들만 모아서 출력 시킴.
+    @GetMapping(value = "/eureka/actuator/info")
+    ResponseEntity getDetailMyClientFromEureka(); //필요한 것들만 모아서 출력 시킴, 자기 자신
 
-    @GetMapping(value = "/eureka/application")
+    @GetMapping(value = "/eureka/applications/info") //(o) 유레카에 등록된 모든 어플리케이션 - 명세는 간단히
     ResponseEntity getApplicationsFromEureka();
 
-    @GetMapping(value = "/eureka/applications/{applicationName}", produces = "application/json") //applications들 중 이름으로 application 명세
-    ResponseEntity getApplicationWithApplicationName(@PathVariable String applicationName);
+    @GetMapping(value = "/eureka/application/{applicationName}/info", produces = "application/json") // (o) applications들 중 이름으로 application 명세, 간단히
+    ResponseEntity getApplicationFromEureka(@PathVariable String applicationName);
+    //AppName, Host, Id, IpAddr, Status
 
-    @GetMapping(value = "/eureka/instances/{instanceId}", produces = "application/json") //eureka instanceId로 명세 찾기
-    ResponseEntity getInstanceDetailFromEureka(@PathVariable("instanceId") String instanceId);
+    @GetMapping(value = "/eureka/applications/{applicationName}/info/detail", produces = "application/json") //eureka instanceId로 명세 찾기
+    ResponseEntity getApplicationDetailFromEureka(@PathVariable("applicationName") String applicationName);
+    //applicationName으로 모든 data 찾기, applicationName 중복 가능? <- 확인 필요, Applications 가 아니고,Application임
 
-    @GetMapping(value = "/eureka/regions/clients", produces = "application/json") //모든 리전 명세
+    @GetMapping(value = "/eureka/regions/info", produces = "application/json")
     ResponseEntity getAnyRegionsFromEureka();
+    // 모든 리전 명세
 
-    @GetMapping(value = "/eureka/regions/{regions}/clients/applications", produces = "application/json")
+    @GetMapping(value = "/eureka/regions/{regions}/applications/info", produces = "application/json")
     ResponseEntity getApplicationsForRegions(@PathVariable("regions") String regions);
 
 }
